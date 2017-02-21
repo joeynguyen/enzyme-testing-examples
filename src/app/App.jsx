000fd630707d9452f11b052f06a55f96bbc64a92
Joey Nguyen <jnn193@gmail.com>
2017-02-21 02:09:32 -0600

    Implement base React setup

---
+++ src/app/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>Enzyme unit testing with React and Redux in ES6</h1>
+      </div>
+    );
+  }
+})
--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-/* eslint-disable no-console */
-console.log('hi');
+import React from 'react';
+import { render } from 'react-dom';
+import App from './app/App.jsx';
+
+render(<App/>, document.getElementById('app'));
